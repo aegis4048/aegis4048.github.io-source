c97f3d0a3e9c01cebed56c59e19330ca8f1c5941
Eric_Kim <aegis4048@gmail.com>
2019-01-04 02:53:31 -0600

    migration

---
+++ pelicanconf.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'ERIC SOOBIN KIM'
+SITENAME = 'Pythonic Excursions'
+SITESUBTITLE = u'Stories about Python and Data Science'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Set the article URL
+ARTICLE_URL = 'articles/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'articles/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#MARKUP = ('md', 'ipynb')
+#PLUGINS = ['ipynb.markup']
+
+MARKUP = ['md']
+PLUGIN_PATHS = ['./plugins', './plugins/pelican-plugins']
+PLUGINS = [
+    'summary',       # auto-summarizing articles
+    'feed_summary',  # use summaries for RSS, not full articles
+    'ipynb.liquid',  # for embedding notebooks
+    'liquid_tags.img',  # embedding images
+    'liquid_tags.video',  # embedding videos
+    'liquid_tags.include_code',  # including code blocks
+    'liquid_tags.literal'
+]
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# for liquid tags
+CODE_DIR = 'downloads/code'
+NOTEBOOK_DIR = 'downloads/notebooks'
+
+# THEME SETTINGS
+THEME = './theme/'
+
+ABOUT_PAGE = '/pages/about.html'
+SHOW_ARCHIVES = True
+SHOW_FEED = False  # Need to address large feeds
+
+ENABLE_MATHJAX = True
+
+STATIC_PATHS = ['images', 'figures', 'videos', 'downloads', 'favicon.ico']
+
+# Footer info
+LICENSE_URL = ""
+LICENSE = ""
---
+++ theme/templates/js/custom.js
@@ -0,0 +1,59 @@
+var COLLAPSE_WIDTH = 575;
+
+function init_navbar () {
+    $('.nav-link').on( {
+        mouseenter: function () {
+            $(this).addClass('hover');
+        },
+        mouseleave: function () {
+            $(this).removeClass('hover');
+        }
+    });
+
+    window.isCollapsed = true;
+    $('.btn-search').on('click', function () {
+        if (window.windowSize > COLLAPSE_WIDTH) {
+            $('#search-box').toggleClass('hidden');
+            $('.navbar-nav').toggleClass('hidden');
+        }
+        window.isCollapsed = !isCollapsed
+    });
+}
+function check_width() {
+    window.windowSize = $(window).outerWidth();
+    if (windowSize < COLLAPSE_WIDTH) {
+        $('#search-box').removeClass('hidden');
+        //$('#search-btn').addClass('hidden');
+        $('.navbar-nav').removeClass('hidden');
+        $('.nav-link').addClass('collapsed');
+        $('#search-form').addClass('collapsed');
+        $('#search-form.row').addClass('collapsed');
+        $('.navbar-title').addClass('collapsed');
+        $('.search-box-div').css('display', '');
+
+    }
+    if (windowSize > COLLAPSE_WIDTH) {
+        $('#search-box').addClass('hidden');
+        //$('#search-btn').removeClass('hidden');
+        $('.navbar-nav').removeClass('hidden');
+        $('.nav-link').removeClass('collapsed');
+        $('#search-form').removeClass('collapsed');
+        $('#search-form.row').removeClass('collapsed');
+        $('.navbar-brand').css('margin-left', '0');
+        $('.navbar-toggler').css('margin-right', '0');
+        $('.navbar-title').removeClass('collapsed');
+        $('.search-box-div').css('display', 'inherit');
+    }
+}
+function jupyter_css() {
+    if (window.windowSize > 486) {
+        $('.prompt').css('width', '69.53px');
+    }
+    $('.rendered_html th').css('background', 'unset')
+}
+$(document).ready(function() {
+    check_width();
+    $(window).resize(check_width);
+    init_navbar();
+    jupyter_css();
+});
