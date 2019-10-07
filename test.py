272d73d7d548144a4142a6755941c49fef8e4407
Kim Eric <aegis4048@gmail.com>
2019-10-07 18:01:19 -0500

    conf int

--- test.py
+++ test.py
@@ -1,3 +1,39 @@
-import pandas as pd
+arr = [1, 2, 2, 3, 1]
 
-x = pd.DataFrame([1, 2, 3])
+
+def degree_finder(arr, search_key=None):
+    count_dict = {x: arr.count(x) for x in arr}
+    for key, val in count_dict.items():
+        if search_key is not None:
+            if val == max(count_dict.values()):
+                return count_dict[key]
+
+
+def degreeOfArray(arr):
+    count_dict = {x: arr.count(x) for x in arr}
+    degree = degree_finder(arr)
+
+    elements = []
+    for key, val in count_dict.items():
+        if val == degree:
+            elements.append(key)
+
+    lengths = []
+    for key in elements:
+        start = 0
+        for i, item in enumerate(arr):
+            if item == key:
+                start = i
+                break
+        sub_arr = arr[start: ]
+        for i, item in enumerate(sub_arr):
+            sub_degree = degree_finder(sub_arr[: i])
+            if sub_degree == degree:
+                lengths.append(i)
+                break
+
+        return min(lengths)
+
+
+
+
