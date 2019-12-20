aed3c06fffd7b4f9dd15e63c9993e7f7e9e61514
Kim Eric <aegis4048@gmail.com>
2019-12-19 18:34:56 -0600

    multicollinearity save

--- test.py
+++ test.py
@@ -1,39 +1,77 @@
-arr = [1, 2, 2, 3, 1]
+import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 
+def generate_collinear_data(cov, size=50, coefs=[0, 0], intercept=0, means=[0, 0, 0], loc=0, scale=1):
 
-def degree_finder(arr, search_key=None):
-    count_dict = {x: arr.count(x) for x in arr}
-    for key, val in count_dict.items():
-        if search_key is not None:
-            if val == max(count_dict.values()):
-                return count_dict[key]
+    # random generation of 3D gaussian collinear features.
+    X = np.random.multivariate_normal(mean=means, cov=cov, size=size)
 
+    print(size)
 
-def degreeOfArray(arr):
-    count_dict = {x: arr.count(x) for x in arr}
-    degree = degree_finder(arr)
+    # generate gaussian white noise.
+    gaussian_noise = np.random.normal(loc=loc, scale=scale, size=size)
 
-    elements = []
-    for key, val in count_dict.items():
-        if val == degree:
-            elements.append(key)
+    # make the outcome.
+    y = intercept + coefs[0] * X[:, 0] + coefs[1] * X[:, 1] + gaussian_noise
 
-    lengths = []
-    for key in elements:
-        start = 0
-        for i, item in enumerate(arr):
-            if item == key:
-                start = i
-                break
-        sub_arr = arr[start: ]
-        for i, item in enumerate(sub_arr):
-            sub_degree = degree_finder(sub_arr[: i])
-            if sub_degree == degree:
-                lengths.append(i)
-                break
+    # train
+    ols = linear_model.LinearRegression()
+    model = ols.fit(X, y)
 
-        return min(lengths)
+    return model.coef_, model.intercept_
 
+# settings
+m = 2000                               # number of simulations
+n = 100                               # sample size
+true_coefs = [13, 0.5]                   # linear regression coefficients, 3 features
+true_intercept = 2                    # y-intercept
+feature_means = [5, 1]            # means of gaussian features. This is not important
+correlation_lo = 0.01                 # degree of collinearity (LOW)
+correlation_hi = 0.95                 # degree of collinearity (HIGH)
 
+# high collinearity covariance matrix
+cov_hi = np.full((len(true_coefs), len(true_coefs)), correlation_hi)
+np.fill_diagonal(cov_hi, 1)
 
+# low collinearity covariance matrix
+cov_lo = np.full((len(true_coefs), len(true_coefs)), correlation_lo)
+np.fill_diagonal(cov_lo, 1)
 
+# simulation
+model_hi_col_coefs = []
+model_lo_col_coefs = []
+for i in range(m):
+    model_hi_col_coefs.append(
+        generate_collinear_data(cov_hi, size=n, coefs=true_coefs, intercept=true_intercept, means=feature_means, loc=0, scale=2)[0]
+    )
+    model_lo_col_coefs.append(
+        generate_collinear_data(cov_lo, size=n, coefs=true_coefs, intercept=true_intercept, means=feature_means, loc=0, scale=2)[0]
+    )
+
+# list to numpy array conversion
+model_hi_col_coefs = np.array(model_hi_col_coefs)
+model_lo_col_coefs = np.array(model_lo_col_coefs)
+
+# plotting
+plt.style.use('default')
+plt.style.use('ggplot')
+
+fig, ax = plt.subplots(figsize=(8, 4.5))
+
+ax.scatter(model_hi_col_coefs[:, 0], model_hi_col_coefs[:, 1], s=10, label='High multicollinearity',
+           edgecolor='red', facecolor='firebrick', alpha=0.7)
+ax.scatter(model_lo_col_coefs[:, 0], model_lo_col_coefs[:, 1], s=10, label='Low multicollinearity',
+           edgecolor='dimgrey', facecolor='k', alpha=0.7)
+
+ax.set_xlim(10.5, 15.5)
+ax.set_ylim(-2, 3)
+ax.set_xlabel('$x_1$ coefficient values', fontsize=16)
+ax.set_ylabel('$x_2$ coefficient values', fontsize=16)
+ax.set_title('Model instability due to multicollinearity', fontsize=20)
+ax.legend(facecolor='white', fontsize=11)
+ax.text(0.2, 0.1, 'aegis4048.github.io', fontsize=13, ha='center', va='center',
+         transform=ax.transAxes, color='grey', alpha=0.5)
+
+fig.tight_layout()
+plt.show()
