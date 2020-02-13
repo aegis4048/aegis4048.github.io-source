d5bba3213b91fa427979257965bdcb56fdd5cc5e
Kim Eric <aegis4048@gmail.com>
2020-02-13 13:14:04 -0600

    job start

--- aaa.py
+++ aaa.py
@@ -1,9 +1,10 @@
 import numpy as np
-from sklearn import linear_model
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
 
 def generate_collinear_data(cov, n=10, true_coefs=[0, 0], true_intercept=0, feature_means=[0, 0, 0], loc=0, scale=1):
-
     # random generation of 3D gaussian collinear features.
     X = np.random.multivariate_normal(mean=feature_means, cov=cov, size=n)
 
@@ -17,65 +18,84 @@ def generate_collinear_data(cov, n=10, true_coefs=[0, 0], true_intercept=0, feat
 
     return X, y
 
+
 # settings
-m = 2000                                 # number of simulations
-correlation_lo = 0.01                    # degree of collinearity (LOW)
-correlation_hi = 0.98                    # degree of collinearity (HIGH)
+m = 10000  # number of simulations
+correlation = 0.98  # degree of collinearity (HIGH)
 
 kwargs = {
-    'n': 100,                            # sample size
-    'true_coefs': [13, 0.5],             # linear regression coefficients, 2 features
-    'true_intercept': 2,                 # y-intercept
-    'feature_means': [0, 0],             # means multivariate normal distribution. This is not important
-    'loc': 0,                            # mean of gaussian noise
-    'scale': 1                           # standard deviation of gaussian noise
+    'n': 1000,  # sample size
+    'true_coefs': [13, 0.5, 5],  # linear regression coefficients, 2 features
+    'true_intercept': 2,  # y-intercept
+    'feature_means': [-12, 14, 2],  # means multivariate normal distribution. This is not important
+    'loc': 0,  # mean of gaussian noise
+    'scale': 1  # standard deviation of gaussian noise
 }
 
 # high collinearity covariance matrix
-cov_hi = np.full((len(kwargs['true_coefs']), len(kwargs['true_coefs'])), correlation_hi)
-np.fill_diagonal(cov_hi, 1)
-
-# low collinearity covariance matrix
-cov_lo = np.full((len(kwargs['true_coefs']), len(kwargs['true_coefs'])), correlation_lo)
-np.fill_diagonal(cov_lo, 1)
+cov = np.full((len(kwargs['true_coefs']), len(kwargs['true_coefs'])), correlation)
+np.fill_diagonal(cov, 1)
 
 # simulation
-model_hi_col_coefs = []
-model_lo_col_coefs = []
+size_hi_params = []
+size_lo_params = []
 for i in range(m):
+    # high collinearity data
+    X_hi, y_hi = generate_collinear_data(cov, **kwargs)
+    interaction_hi_1 = X_hi[:, 0].reshape(-1, 1) ** 2  # x1^2
+    interaction_hi_2 = X_hi[:, 1].reshape(-1, 1) * X_hi[:, 2].reshape(-1, 1)  # x2 * x3
+    X_hi = np.concatenate([X_hi, interaction_hi_1, interaction_hi_2], axis=1)
+    X_st_hi = sm.add_constant(X_hi)
+    model_hi = sm.OLS(y_hi, X_st_hi).fit()
+    size_hi_params.append(model_hi.params)
 
-    X_hi, y_hi = generate_collinear_data(cov_hi, **kwargs)
-    ols_hi = linear_model.LinearRegression()
-    model_hi = ols_hi.fit(X_hi, y_hi)
-    model_hi_col_coefs.append(model_hi.coef_)
+    # low collinearity data
+    X_lo, y_lo = generate_collinear_data(cov, **kwargs)
+    interaction_lo_1 = X_lo[:, 0].reshape(-1, 1) ** 2
+    interaction_lo_2 = X_lo[:, 1].reshape(-1, 1) * X_lo[:, 2].reshape(-1, 1)
+    X_lo = np.concatenate([X_lo, interaction_lo_1, interaction_lo_2], axis=1)
+    scaler = StandardScaler(with_std=False)
+    X_lo, y_lo = scaler.fit_transform(X_lo), scaler.fit_transform(y_lo.reshape(-1, 1)).flatten()
+    X_st_lo = sm.add_constant(X_lo)
+    model_lo = sm.OLS(y_lo, X_st_lo).fit()
+    size_lo_params.append(model_lo.params)
 
-    X_lo, y_lo = generate_collinear_data(cov_lo, **kwargs)
-    ols_lo = linear_model.LinearRegression()
-    model_lo = ols_lo.fit(X_lo, y_lo)
-    model_lo_col_coefs.append(model_lo.coef_)
+# list to numpy conversion
+size_hi_params = np.asarray(size_hi_params)
+size_lo_params = np.asarray(size_lo_params)
 
-# list to numpy array conversion
-model_hi_col_coefs = np.array(model_hi_col_coefs)
-model_lo_col_coefs = np.array(model_lo_col_coefs)
 
 # plotting
-plt.style.use('default')
-plt.style.use('ggplot')
+def styling(ax):
+    ax.set_facecolor('#eeeeee')
+    ax.grid(True, linestyle='--', color='#acacac')
+    ax.tick_params(color='grey')
+    _ = [spine.set_edgecolor('grey') for spine in ax.spines.values()]
+    ax.text(0.5, 0.1, 'aegis4048.github.io', fontsize=12, ha='center', va='center',
+            transform=ax.transAxes, color='grey', alpha=0.5)
 
-fig, ax = plt.subplots(figsize=(8, 4.5))
 
-ax.scatter(model_hi_col_coefs[:, 0], model_hi_col_coefs[:, 1], s=10, label='High multicollinearity',
-           edgecolor='red', facecolor='firebrick', alpha=0.7)
-ax.scatter(model_lo_col_coefs[:, 0], model_lo_col_coefs[:, 1], s=10, label='Low multicollinearity',
-           edgecolor='dimgrey', facecolor='k', alpha=0.7)
+boxplot_styling = {
+    'sym': '',
+    'whis': [2.5, 97.5],
+    'showfliers': False,
+    'boxprops': dict(linewidth=2.0, color='#4e98c3'),
+    'whiskerprops': dict(linewidth=2.0, color='#4e98c3', linestyle='--'),
+    'vert': True,
+    'capprops': dict(linewidth=2.0, color='k'),
+    'medianprops': dict(linewidth=2.0, color='#ad203e'),
+    'widths': (0.4, 0.4)
+}
 
-ax.set_xlim(10.5, 15.5)
-ax.set_ylim(-2, 3)
-ax.set_xlabel('$x_1$ coefficient values', fontsize=16)
-ax.set_ylabel('$x_2$ coefficient values', fontsize=16)
-ax.set_title('Model instability due to multicollinearity', fontsize=20)
-ax.legend(facecolor='white', fontsize=11)
-ax.text(0.2, 0.1, 'aegis4048.github.io', fontsize=13, ha='center', va='center',
-         transform=ax.transAxes, color='grey', alpha=0.5)
+labels = ['Intercept', '$X_1$', '$X_2$', '$X_3$', '$X_1^2$', '$X_2X_3$']
 
-fig.tight_layout()
+fig, axes = plt.subplots(1, len(labels), figsize=(16, 6))
+
+for i, (ax, label) in enumerate(zip(axes, labels)):
+    ax.boxplot([list(size_hi_params[:, i]), list(size_lo_params[:, i])], **boxplot_styling)
+    styling(ax)
+    ax.set_title(label, fontsize=20, y=-0.2)
+    ax.set_xticklabels(['n = %.d' % 1, 'n = %.d' % 2], fontsize=15)
+
+fig.tight_layout(rect=[0, 0.05, 1, 0.91])
+fig.suptitle('Range of coefficients with big & small sample sizes - %.f simulations' % m, fontsize=25);
